5b2286fe715a26db1fad683ac30b1423092644c2
Iivari Anttila <157590564+iivariepic@users.noreply.github.com>
2025-05-07 15:37:53 +0300

    Delete part1/lecture_examples directory

--- part1/lecture_examples/README.md
+++
@@ -1,2 +0,0 @@
-# This directory does not contain exercises
-This is just the directory I used for following along with the lectures
--- part1/lecture_examples/node/example.js
+++
@@ -1,9 +0,0 @@
-const arto = {
-  name: 'Arto Hellas',
-  greet: function() {
-    console.log('hello, my name is ' + this.name)
-  },
-}
-
-
-setTimeout(arto.greet, 1000)
