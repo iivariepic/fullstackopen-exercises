95fecc72c9dab06b7c54df0402c367576f0df5fd
Iivari Anttila <iivari03@gmail.com>
2025-05-09 12:48:09 +0300

    Exercise 2.1

---
+++ part2/courseinfo/README.md
@@ -0,0 +1,12 @@
+# Course information
+Updated version of the course information app created in part 1, this was made in the following exercises:
+
+Exercise 2.1
+<br>
+Exercise 2.2
+<br>
+Exercise 2.3
+<br>
+Exercise 2.4
+<br>
+Exercise 2.5
---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,60 @@
+const Header = ({ text }) => <h1>{text}</h1>
+
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
+
+const Content = ({ course }) => {
+  return (
+    <>
+      {course.parts.map((part, index) => (
+        <Part key={index} part={part}/>
+      ))
+      }
+    </>
+  )
+}
+
+const Total = ({ course }) => {
+  const total = course.parts.reduce((n, {exercises}) => n + exercises, 0);
+  return (
+    <p>
+      Number of exercises {total}
+    </p>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header text={course.name}/>
+      <Content course={course}/>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
