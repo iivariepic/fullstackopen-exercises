64389afb014b687bfeb19f72e998bfff3cc56728
Iivari Anttila <iivari03@gmail.com>
2025-05-12 15:20:33 +0300

    Exercise 2.19

--- part2/countrysearch/src/components/CountryList.jsx
+++ part2/countrysearch/src/components/CountryList.jsx
@@ -1,18 +1,41 @@
 import SingleCountryInformation from "./SingleCountryInformation";
+import {useState, useEffect} from "react";
 
-const CountryListEntry = ({ country }) => <li>{country.name.common}</li>
+const ShowCountryButton = ({ onClick }) => <button onClick={onClick}>Show</button>
+
+const CountryListEntry = ({ country, onShow }) => <li>{country.name.common} <ShowCountryButton
+  onClick={() => onShow(country)}/></li>
+
 
 const CountryList = ({ countries, searchQuery }) => {
+  const [selectedCountry, setSelectedCountry] = useState(null)
+
+  // Reset the selection if the search query was changed
+  useEffect(() => {
+    setSelectedCountry(null);
+  }, [searchQuery]);
+
   if (countries === null || searchQuery === null) return <p>Loading, please wait...</p>
 
   const filteredCountries = countries.filter(country =>
     country.name.common.toLowerCase().includes(searchQuery.toLowerCase())
   )
 
+  if (selectedCountry) {
+    return (
+      <SingleCountryInformation country={selectedCountry}/>
+    )
+  }
+
   return (
     <>
       {filteredCountries.length > 1 && filteredCountries.length <= 10?
-        filteredCountries.map(country => <CountryListEntry key={country.name.common} country={country}/> )
+        filteredCountries.map(country => <CountryListEntry
+          key={country.name.common}
+          country={country}
+          onShow={setSelectedCountry}
+          />
+        )
       : filteredCountries.length > 10 ? <li>Too many matches, specify another filter</li>
       : filteredCountries.length === 1 ? <SingleCountryInformation country={filteredCountries[0]}/>
       : <li>No matches</li>}
