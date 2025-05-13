735568002bf840e6ef1e06f47dc613bef6113197
Iivari Anttila <iivari03@gmail.com>
2025-05-13 15:47:26 +0300

    Exercise 3.9

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('post-data', (req, res) => {
@@ -11,6 +12,8 @@ morgan.token('post-data', (req, res) => {
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data'))
+app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
   {
@@ -98,7 +101,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
--- part3/phonebook_backend/readme.md
+++ part3/phonebook_backend/readme.md
@@ -1,4 +1,5 @@
 # Phonebook backend
+(As of exercise 3.9, this contains the dist file from the front-end build as well)<br>
 This is a phonebook app made with node.js and was created in the following exercises:
 
 Exercise 3.1
@@ -16,3 +17,5 @@ Exercise 3.6
 Exercise 3.7
 <br>
 Exercise 3.8
+<br>
+Exercise 3.9
