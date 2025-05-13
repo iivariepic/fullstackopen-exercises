805ac85f1bdd21e8a9d1528923e208a0e682f5b0
Iivari Anttila <iivari03@gmail.com>
2025-05-13 13:48:09 +0300

    Exercise 3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -32,6 +32,18 @@ app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+  response.json(person)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
