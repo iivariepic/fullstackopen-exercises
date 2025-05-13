f2f501f7be27f6a49741edd2bd0961ee6a415f6f
Iivari Anttila <iivari03@gmail.com>
2025-05-13 14:50:05 +0300

    Exercise 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,8 +2,15 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+morgan.token('post-data', (req, res) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  }
+  return ''
+})
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data'))
 
 let persons = [
   {
