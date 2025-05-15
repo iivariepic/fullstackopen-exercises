c0781e6483dda1545170ffcac80557f94b4ee8d6
Iivari Anttila <iivari03@gmail.com>
2025-05-15 14:20:24 +0300

    Exercise 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,12 +1,14 @@
-const dummy = (blogs) => {
-  return 1
-}
+const dummy = (blogs) => 1
 
-const totalLikes = (blogs) => {
-  return blogs.reduce((total, blog) => total + blog.likes, 0)
-}
+const totalLikes = (blogs) => blogs.reduce((total, blog) => total + blog.likes, 0)
+
+
+const favouriteBlog = (blogs) => blogs.length > 0
+    ? blogs.reduce((favourite, blog) => (blog.likes > favourite.likes ? blog: favourite))
+    : null
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 }
