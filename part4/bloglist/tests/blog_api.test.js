a122aabecc5d998364286e6ed72bb78274759a5d
Iivari Anttila <iivari03@gmail.com>
2025-05-27 22:36:08 +0300

    Exercise 4.9

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,15 +1,36 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const assert = require('node:assert')
 const app = require('../app')
+const Blog = require('../models/blog')
+const { blogs } = require('./dummy_lists')
 
 const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  for (const blog of blogs) {
+    const blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
 
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('unique identifier is named \'id\'', async () => {
+  const response = await api.get('/api/blogs')
+  const blogs = response.body
+
+  for (const blog of blogs) {
+    assert.ok(blog.id)
+    assert.strictEqual(blog._id, undefined)
+  }
 })
 
 after(async () => {
