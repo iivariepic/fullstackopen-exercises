2f7ac7cdc840fa3ea1464d02f9a2543c9294630e
Iivari Anttila <iivari03@gmail.com>
2025-05-28 14:30:12 +0300

    Exercise 4.15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,9 +3,10 @@ const app = express()
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 mongoose.connect(config.MONGODB_URI)
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
-
+app.use('/api/users', usersRouter)
 module.exports = app
