b9d0a86f3c69c235667c83d53b15f11bb1736700
Iivari Anttila <iivari03@gmail.com>
2025-05-28 15:40:10 +0300

    Exercise 4.19

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/',  async (request, response) => {
   const blogs = await Blog
@@ -11,14 +20,37 @@ blogsRouter.get('/',  async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   try {
-    const randomUser = await User.findOne({})
-    const blog = new Blog(request.body)
-    blog.user = randomUser
+    const body = request.body
+
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+
+    if (!user) {
+      return response.status(400).json({ error: 'UserId missing or not valid' })
+    }
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      user: user._id
+    })
+
     const result = await blog.save()
+    user.blogs = user.blogs.concat(result._id)
+    await user.save()
+
     response.status(201).json(result)
   } catch (error) {
     if (error.name === 'ValidationError') {
       return response.status(400).send(error.message)
+    }
+    if (error.name === 'JsonWebTokenError') {
+      return response.status(401).send(error.message)
     }
     response.status(500).end()
   }
