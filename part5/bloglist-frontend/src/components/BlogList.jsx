4eaa96c6066758c9c5541687b8622ae71dbeb0f6
Iivari Anttila <iivari03@gmail.com>
2025-06-04 18:30:49 +0300

    Exercise 5.10

--- part5/bloglist-frontend/src/components/BlogList.jsx
+++ part5/bloglist-frontend/src/components/BlogList.jsx
@@ -1,9 +1,11 @@
 import Blog from '../components/Blog'
 
 const BlogList = ({ blogs }) => {
+  const sortedBlogs = [...blogs].sort((a, b) => b.likes - a.likes)
+
   return (
     <div>
-      {blogs.map(blog =>
+      {sortedBlogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
