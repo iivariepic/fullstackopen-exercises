a058c1f0b4a8e5dabf3bf70ab5598ddcc425cacf
Iivari Anttila <iivari03@gmail.com>
2025-06-04 18:06:22 +0300

    Exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,29 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false)
+
+  const collapsedView =
+    <div className="blog">
+      {blog.title} {blog.author} <button onClick={() => setExpanded(true)}> view </button>
+    </div>
+
+  const expandedView =
+    <div className="blog">
+      <div>{blog.title} {blog.author} <button onClick={() => setExpanded(false)}> hide </button></div>
+      <div>{blog.url}</div>
+      <div>likes {blog.likes} <button> like </button></div>
+      <div>{blog.user.name}</div>
+    </div>
+
+  return (
+    <>
+      {expanded
+        ? expandedView
+        : collapsedView}
+    </>
+  )
+}
+
 
 export default Blog
