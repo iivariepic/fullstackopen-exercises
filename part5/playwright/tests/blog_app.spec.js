0a75aaa058b4db2ceeb9cf6aa38f346c93bb86eb
Iivari Anttila <iivari03@gmail.com>
2025-06-09 13:56:05 +0300

    Exercise 5.17

---
+++ part5/playwright/tests/blog_app.spec.js
@@ -0,0 +1,14 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    const locator = await page.getByText('blogs')
+    await expect(locator).toBeVisible()
+    await expect(page.getByText('username')).toBeVisible()
+    await expect(page.getByText('password')).toBeVisible()
+  })
+})
