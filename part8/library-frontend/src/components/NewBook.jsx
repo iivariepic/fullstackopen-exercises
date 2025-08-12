21aa72a6cfd2c0fd8cb7900a7ae68f7c81d3b2fd
Iivari Anttila <iivari03@gmail.com>
2025-08-12 16:21:34 +0300

    Exercise 8.10

--- part8/library-frontend/src/components/NewBook.jsx
+++ part8/library-frontend/src/components/NewBook.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import { useMutation } from "@apollo/client"
+import { ALL_BOOKS, CREATE_BOOK } from "../queries/books"
+import { ALL_AUTHORS } from "../queries/authors.js"
 
 const NewBook = () => {
   const [title, setTitle] = useState('')
@@ -7,10 +10,14 @@ const NewBook = () => {
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
 
+  const [ createBook ] = useMutation(CREATE_BOOK, {
+    refetchQueries: [ { query: ALL_BOOKS }, { query: ALL_AUTHORS } ]
+  })
+
   const submit = async (event) => {
     event.preventDefault()
 
-    console.log('add book...')
+    await createBook({ variables: { title, author, published: parseInt(published), genres } })
 
     setTitle('')
     setPublished('')
