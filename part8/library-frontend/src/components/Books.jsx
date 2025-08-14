30571858b251cfd24d8c012b22ed639f42f05b68
Iivari Anttila <iivari03@gmail.com>
2025-08-14 14:15:03 +0300

    Exercise 8.19 & 8.21

--- part8/library-frontend/src/components/Books.jsx
+++ part8/library-frontend/src/components/Books.jsx
@@ -1,33 +1,59 @@
+import { useState } from "react"
 import { useQuery } from "@apollo/client"
-import { ALL_BOOKS } from "../queries/books"
+import { ALL_BOOKS, ALL_GENRES } from "../queries/books"
+
+const GenreSelector = ({ allGenres, setGenre }) => {
+  return (
+    <div>
+      {allGenres.map(genre =>
+        <button onClick={() => setGenre(genre)} key={genre}>{genre}</button>
+      )}
+      <button onClick={() => setGenre(undefined)} key={"all"}>all genres</button>
+    </div>
+  )
+}
+
+const BooksTable = ({ books }) => {
+  return (
+    <table>
+      <tbody>
+      <tr>
+        <th></th>
+        <th>author</th>
+        <th>published</th>
+      </tr>
+      {books.map((a) => (
+        <tr key={a.title}>
+          <td>{a.title}</td>
+          <td>{a.author.name}</td>
+          <td>{a.published}</td>
+        </tr>
+      ))}
+      </tbody>
+    </table>
+  )
+}
 
 const Books = () => {
-  const result = useQuery(ALL_BOOKS)
+  const [genre, setGenre] = useState(undefined)
+  const resultGenres = useQuery(ALL_GENRES)
+  const resultFiltered = useQuery(ALL_BOOKS, {
+    variables: { genre },
+    fetchPolicy: "cache-and-network",
+    notifyOnNetworkStatusChange: true
+  })
 
-  if (result.loading) return <div>loading...</div>
-
-  const books = result.data.allBooks
+  const genreData = resultGenres.data?.allBooks ?? []
+  const allGenres = [...new Set(genreData.flatMap(book => book.genres))]
+  const filteredBooks = resultFiltered.data?.allBooks  ?? resultFiltered.previousData?.allBooks ?? []
 
   return (
     <div>
       <h2>books</h2>
 
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>author</th>
-            <th>published</th>
-          </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      {genre ? <p>in genre <b>{genre}</b></p> : <p><b>all genres</b></p>}
+      <BooksTable books={filteredBooks} />
+      <GenreSelector allGenres={[...allGenres]} setGenre={setGenre} />
     </div>
   )
 }
--- part8/library-frontend/src/queries/books.js
+++ part8/library-frontend/src/queries/books.js
@@ -1,13 +1,24 @@
 import { gql } from '@apollo/client'
 
 export const ALL_BOOKS = gql`
-query {
-  allBooks {
+query getAllByFilterBooks($genre: String){
+  allBooks(
+      genre: $genre
+  ) {
     title
     author {
         name
     }
     published
+    genres
+  }
+}
+`
+
+export const ALL_GENRES = gql`
+{
+  allBooks {
+      genres
   }
 }
 `
