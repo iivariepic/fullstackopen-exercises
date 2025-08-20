f753bf4f1ed087e83b9d520e5604e226c255aea2
Iivari Anttila <iivari03@gmail.com>
2025-08-20 14:58:47 +0300

    Exercise 9.10

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,13 +1,19 @@
 import express from 'express';
+
+import diagnosisRouter from './routes/diagnoses'
 const app = express();
 app.use(express.json());
 
-const PORT = 3003;
+const PORT = 3000;
 
 app.get('/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+
+app.use('/api/diagnoses', diagnosisRouter);
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
