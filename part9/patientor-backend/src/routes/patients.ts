506513effacc37c3818aade65ee94a0ff5c89e51
Iivari Anttila <iivari03@gmail.com>
2025-08-20 15:28:47 +0300

    Exercise 9.12

--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -1,10 +1,17 @@
 import express from 'express';
 import patientService from '../services/patientService'
+import { Patient } from "../types"
 
 const router = express.Router();
 
 router.get('/', (_req, res) => {
   res.send(patientService.getAllWithoutSsn())
+});
+
+router.post('/', (req, res) => {
+  const patient: Omit<Patient, 'id'> = req.body
+  const addedPatient = patientService.create(patient)
+  res.json(addedPatient)
 });
 
 export default router;
--- part9/patientor-backend/src/services/patientService.ts
+++ part9/patientor-backend/src/services/patientService.ts
@@ -1,10 +1,23 @@
+import { v4 as uuidv4 } from 'uuid';
 import patientData from '../../data/patients';
 import { Patient } from "../types"
 
+const patients: Patient[] = patientData
+
 const getAllWithoutSsn = (): Omit<Patient, 'ssn'>[] => {
-  return patientData.map(({ ssn, ...rest }) => rest);
+  return patients.map(({ ssn, ...rest }) => rest);
 };
 
+const create = (patient: Omit<Patient, 'id'>)=> {
+  const newPatient = {
+    id: uuidv4(),
+    ...patient
+  }
+  patients.push(newPatient)
+  return newPatient
+}
+
 export default {
-  getAllWithoutSsn
+  getAllWithoutSsn,
+  create
 };
