00d9ab76decf3d886ed2831699b48bdba01bf20f
Iivari Anttila <iivari03@gmail.com>
2025-08-22 13:08:19 +0300

    Exercise 9.25

--- part9/patientor-frontend/src/components/PatientPage/EntriesList.tsx
+++ part9/patientor-frontend/src/components/PatientPage/EntriesList.tsx
@@ -1,10 +1,46 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Typography, Box } from "@mui/material"
-import { Entry } from "../../types.ts"
+import { Diagnosis, Entry } from "../../types.ts"
 import PatientTable from "./PatientTable.tsx"
+import diagnosisService from "../../services/diagnoses";
 
 interface Props {
   entries: Entry[]
+}
+
+const EntryTable = ({ entry }: { entry: Entry }) => {
+  const tableTitles = ["Date", "Description"]
+  const tableData: (string | React.ReactNode)[] = [entry.date, entry.description]
+  const [diagnoses, setDiagnoses] = useState<Diagnosis[]>([])
+
+  useEffect(() => {
+    const fetchDiagnoses = async () => {
+      const allDiagnoses = await diagnosisService.getAll()
+      setDiagnoses(allDiagnoses)
+    }
+    fetchDiagnoses()
+  }, [])
+
+  const diagnosesMap = Object.fromEntries(diagnoses.map(d => [d.code, d]))
+
+  if (entry.diagnosisCodes) {
+    tableTitles.push("Diagnosis Codes")
+    tableData.push(
+      <ul style={{ margin: 0, paddingLeft: "1.2em" }}>
+        {entry.diagnosisCodes.map(code => (
+          <li key={code}>
+            <b>{code}</b>: {diagnosesMap[code]?.name || ""}
+          </li>
+        ))}
+      </ul>
+    )
+  }
+  return (
+    <PatientTable
+      tableTitles={tableTitles}
+      tableData={tableData}
+    />
+  )
 }
 
 const EntriesList = ({ entries }: Props) => {
@@ -19,26 +55,7 @@ const EntriesList = ({ entries }: Props) => {
       >
         Entries
       </Typography>
-      {entries.map(entry => {
-        const tableTitles = ["Date", "Description"]
-        const tableData: (string | React.ReactNode)[] = [entry.date, entry.description]
-        if (entry.diagnosisCodes) {
-          tableTitles.push("Diagnosis Codes")
-          tableData.push(
-            <ul style={{ margin: 0, paddingLeft: "1.2em" }}>
-              {entry.diagnosisCodes.map(code => (
-                <li key={code}>{code}</li>
-              ))}
-            </ul>
-          )
-        }
-        return (
-          <PatientTable
-            tableTitles={tableTitles}
-            tableData={tableData}
-          />
-        )
-      })}
+      {entries.map(entry => <EntryTable entry={entry} />)}
       {entries.length === 0 &&
         <Typography
           sx={{
